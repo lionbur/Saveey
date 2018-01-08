b30f30c28f86041385df5ecfcdcd72f914ade4e9
Lion Burger <lionb@welldone-software.com>
2018-01-08 14:50:19 +0200

    Name component to use ellipsis and line clamping

--- src/js/inject/results/list/Name.js
+++ src/js/inject/results/list/Name.js
@@ -5,6 +5,13 @@ import _Highlighter from 'react-highlight-words'
 const Highlighter = styled(_Highlighter)`
   flex: 1;
   padding: 0 5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  max-height: 64px;
+  line-height: 20px;
 `
 
 export default ({ children, filter, ...rest }) => (
